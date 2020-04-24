3a253599a2d8cc119415a3916025978f57658b39
Lesley Ayemi <leeayemi@gmail.com>
2020-04-24 15:37:55 +0100

    create category feature added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,6 +29,8 @@ Route::group(['middleware' => 'admin'], function () {
 
     Route::resource('admin/users', 'AdminUsersController');
     Route::resource('admin/posts', 'AdminPostsController');
+    Route::resource('admin/categories', 'AdminCategoriesController');
+
 
     
 });
