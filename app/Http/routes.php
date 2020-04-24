fa45f4a4a22cdced0e277869cbe7a4e32b54a4f8
Lesley Ayemi <leeayemi@gmail.com>
2020-04-25 00:19:56 +0100

    setting up the comment system

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,6 +32,9 @@ Route::group(['middleware' => 'admin'], function () {
     Route::resource('admin/categories', 'AdminCategoriesController');
     Route::resource('admin/media', 'AdminMediasController');
 
+    Route::resource('admin/comments', 'PostCommentsController');
+    Route::resource('admin/comment/replies', 'CommentRepliesController');
+
 
 
     
